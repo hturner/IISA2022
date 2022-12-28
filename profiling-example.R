48fa22561848baebc782359e9126b266ca911511
Heather Turner <ht@heatherturner.net>
2022-12-28 12:04:52 +0530

    switch parallel section for profiling section

---
+++ profiling-example.R
@@ -0,0 +1,12 @@
+top_level <- function() {
+  profvis::pause(0.1)
+  middle_level()
+  bottom_level()
+}
+middle_level <- function() {
+  profvis::pause(0.1)
+  bottom_level()
+}
+bottom_level <- function() {
+  profvis::pause(0.1)
+}
